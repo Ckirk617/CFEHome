8e1f40aa5637af856808b20a9e6631ddb63c47fd
Christina Kirk <cmariekirk@Christinas-MBP.hsd1.ma.comcast.net>
2018-07-26 16:24:35 -0400

    fixing allowed host issues

--- ttdirectory/settings.py
+++ ttdirectory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w9_!uc9gy2et(0jak6xq0(^awaeyn^8)2@uy4694m(rlbbs%7&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
