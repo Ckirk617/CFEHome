5f9924140823c7ce6281c6d1012918974f55d417
Christina Kirk <cmariekirk@Christinas-MBP.hsd1.ma.comcast.net>
2018-07-26 18:30:58 -0400

    added new home template

--- ttdirectory/views.py
+++ ttdirectory/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'ttdirectory/index.html')
 
 def detail(request, title):
     return HttpResponse("You're looking at question %s." % title)
